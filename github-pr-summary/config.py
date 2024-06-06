3cded95f75844ad37ef5b367ac4b148503911d27
Miguel Angel Briseno Flores <miguel.briseno@oracle.com>
2024-06-06 12:05:18 -0600

    update: refactor and commented the configuration file

--- github-pr-summary/config.py
+++ github-pr-summary/config.py
@@ -1,7 +1,10 @@
 # Configuration details
+
+# Repository Configuration details
 REPO_OWNER = 'huggingface'
 REPO_NAME = 'text-generation-inference'
+
+# Email Information for the Scrum Master
 EMAIL_FROM = 'no-reply@someemail.com'
 EMAIL_TO = 'tosomeone@someemail.com'
 EMAIL_SUBJECT = f'Weekly PR Summary for {REPO_NAME}'
-GITHUB_API_URL = f'https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/pulls'
