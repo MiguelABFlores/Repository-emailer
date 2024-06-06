b342aaff4ae8c0d49e29aa867741ae6da831234b
Miguel Angel Briseno Flores <miguel.briseno@oracle.com>
2024-06-06 14:59:43 -0600

    update: added the merged lists to the email formatter

--- github-pr-summary/email_formatter.py
+++ github-pr-summary/email_formatter.py
@@ -1,9 +1,10 @@
-def format_email_body(repo_name, opened_prs, closed_prs):
+def format_email_body(repo_name, opened_prs, merged_prs, closed_prs):
     # Format the email body with the pull requests count
     # To count the number of items we are just using len which calculates the length of a list given
     # So we are just pointing to the lists of opened and closed prs
     email_body = f"Weekly Pull Request Summary for {repo_name} repository\n\n"
     email_body += f"Total PRs opened in the last week: {len(opened_prs)}\n"
+    email_body += f"Total PRs merged in the last week: {len(merged_prs)}\n"
     email_body += f"Total PRs closed in the last week: {len(closed_prs)}\n\n"
 
     # List all the opened Pull Requests
@@ -11,6 +12,11 @@ def format_email_body(repo_name, opened_prs, closed_prs):
     # I want to make it as a list so it will be initiated with a -
     email_body += "Opened Pull Requests:\n"
     for pr in opened_prs:
+        email_body += f"- title: {pr['title']} | by: {pr['user']['login']} | number: #{pr['number']}\n"
+        email_body += f"  url: {pr['html_url']}\n"
+
+    email_body += "\nMerged Pull Requests:\n"
+    for pr in merged_prs:
         email_body += f"- title: {pr['title']} | by: {pr['user']['login']} | number: #{pr['number']}\n"
         email_body += f"  url: {pr['html_url']}\n"
 
