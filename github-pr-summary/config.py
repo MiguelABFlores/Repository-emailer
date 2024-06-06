803b27c0bea854c700a173a06a367f282115cdc2
Miguel Angel Briseno Flores <miguel.briseno@oracle.com>
2024-06-06 12:24:17 -0600

    comment: moved comments from main to config

--- github-pr-summary/config.py
+++ github-pr-summary/config.py
@@ -8,3 +8,11 @@ REPO_NAME = 'text-generation-inference'
 EMAIL_FROM = 'no-reply@someemail.com'
 EMAIL_TO = 'tosomeone@someemail.com'
 EMAIL_SUBJECT = f'Weekly PR Summary for {REPO_NAME}'
+
+'''
+Chosen public active repository:
+https://github.com/huggingface/text-generation-inference
+https://api.github.com/repos/huggingface/text-generation-inference/pulls
+Getting the json from this we can see what to expect from different parameters
+PR for testing: https://github.com/huggingface/text-generation-inference/pull/1985
+'''
