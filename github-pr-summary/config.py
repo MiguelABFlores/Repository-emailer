6342b8871e09e69ec833f3cdf892337ae0b459f7
Miguel Angel Briseno Flores <miguel.briseno@oracle.com>
2024-06-06 11:57:43 -0600

    update: added the configurable parameters for the project

---
+++ github-pr-summary/config.py
@@ -0,0 +1,7 @@
+# Configuration details
+REPO_OWNER = 'huggingface'
+REPO_NAME = 'text-generation-inference'
+EMAIL_FROM = 'no-reply@someemail.com'
+EMAIL_TO = 'tosomeone@someemail.com'
+EMAIL_SUBJECT = f'Weekly PR Summary for {REPO_NAME}'
+GITHUB_API_URL = f'https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/pulls'
