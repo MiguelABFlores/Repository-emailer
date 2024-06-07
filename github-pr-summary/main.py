46de2a7246bb91f6664e4563180c1a027c6b4dfb
Miguel Angel Briseno Flores <miguel.briseno@oracle.com>
2024-06-06 21:25:05 -0600

    update: changed calling the function for the console

--- github-pr-summary/main.py
+++ github-pr-summary/main.py
@@ -1,7 +1,10 @@
 # Importing from the modular files of the project
 from github_api import fetch_pull_requests
-from email_formatter import format_email_body
+from console_formatter import format_console_body
+from html_formatter import format_html_body
+from html_generator import generate_html_report
 from config import REPO_OWNER, REPO_NAME, EMAIL_FROM, EMAIL_TO, EMAIL_SUBJECT
+
 
 def main():
     # Fetching the json from the repository and calling the function
@@ -12,13 +15,13 @@ def main():
     closed_prs = [pr for pr in closed_prs if pr['merged_at'] is None]
 
     # Format the email body with the pull request data
-    email_body = format_email_body(REPO_NAME, opened_prs, merged_prs, closed_prs)
+    console_body = format_console_body(REPO_NAME, opened_prs, merged_prs, closed_prs)
 
     # Printing the email content to console
     print("From:", EMAIL_FROM)
     print("To:", EMAIL_TO)
     print("Subject:", EMAIL_SUBJECT)
-    print("\nBody:\n", email_body)
+    print("\nBody:\n", console_body)
 
 if __name__ == "__main__":
     main()
